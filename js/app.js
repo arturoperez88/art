570038e3befc5543befed8a0b57e94b57c56c2ad
Arturo Perez <arturoperez.88@live.com>
2017-01-27 22:31:41 -0800

    initial commit

---
+++ js/app.js
@@ -0,0 +1,30 @@
+var windowWidth = $(window).width();
+
+if (windowWidth < 760 ) {
+$(".bun").on('click',function(e){
+	e.preventDefault();
+  $('.menu').slideToggle(500);
+
+  $(this).toggleClass('exit');
+});
+
+$(".project-name").click(function(){
+	$(this).siblings().slideToggle(500);
+	$(this).toggleClass("border-radius","move");
+});
+}else{}
+
+$(window).resize(function() {
+    $('#sub-header').height($(window).height());
+});
+$(window).trigger('resize');
+
+// $(function(){
+// 	setTimeout(function(){
+// 		$('.fly-in-text').removeClass('hidden');
+// 	}, 500);
+		
+// 		setTimeout(function(){
+// 			$('.fly-in-text').fadeOut(1500);
+// 		}, 4000);	
+// });
